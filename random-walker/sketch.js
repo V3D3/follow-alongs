04707ddb514f0994906f426568beae94524481a9
Vedaant Arya (V3D3) <vedaant.arya.v3d3@gmail.com>
2021-10-15 15:06:14 +0530

    Added some controls to random walker. Split into two modes.

--- random-walker/sketch.js
+++ random-walker/sketch.js
@@ -3,10 +3,19 @@ let walker;
 function setup()  {
     createCanvas(400, 400);
     walker = new Walker(200, 200);
+    frameRate(144);
     background(0);
 }
 
 function draw()  {
     walker.update();
     walker.show();
+}
+
+function resetSketch(fmode, fsize)  {
+    background(0);
+    walker = new Walker(200, 200);
+    walker.fixedmode = fmode;
+    walker.fixedsize = fsize;
+    walker.radius    = fsize;
 }
--- random-walker/walker.js
+++ random-walker/walker.js
@@ -1,11 +1,26 @@
 class Walker  {
     constructor(x, y)  {
+        this.fixedmode = true;
+        this.fixedsize = 1;
+        this.radius    = 1;
         this.pos = createVector(x, y);
     }
 
     update()  {
-        this.pos.x = this.pos.x + random(-1, 1);
-        this.pos.y = this.pos.y + random(-1, 1);
+        let dirvec;
+
+        if(this.fixedmode)  {
+            let x = random(-1, 1);
+            let y = random(-1, 1);
+            dirvec = createVector(x, y);
+            dirvec.normalize().mult(this.fixedsize);
+        }  else  {
+            let x = random(-this.radius, this.radius);
+            let y = random(-this.radius, this.radius);
+            dirvec = createVector(x, y);
+        }
+
+        this.pos.add(dirvec);
     }
 
     show()  {
